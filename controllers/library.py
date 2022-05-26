be267e175b5bcddb425998b5be8b28bc822920db
jeffplata <jeffplata@yahoo.com>
2022-05-26 17:03:47 +0800

    dev 5/26

--- controllers/library.py
+++ controllers/library.py
@@ -8,11 +8,21 @@ def index(): return dict(message="hello from library.py")
 #     grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
 #     return locals()
 
+def m_ondelete(table, id):
+	if table == db.region:
+		branches = db(db.branch.region_id==id).select().first()
+		org_accesses = db(db.org_access.region_id==id).select().first()
+		if branches or org_accesses:
+			# response.flash('Cannot delete this record')
+			# redirect(URL('library', 'grid', args='region', vars=dict(title='Regions')))
+			response.flash('Cannot delete this record.')
+
+
 @auth.requires_membership('admin')
 def grid():
     response.view = 'library/edit_record.html'
     title = request.vars['title']
     tablename = request.args(0)
     if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=True, editable=True)
+    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=True, editable=True, ondelete=m_ondelete)
     return dict(grid=grid, title=title)
